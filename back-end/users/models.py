478e5816cfc5441ba85ea263cc0d400c828b54cd
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-04-13 15:22:16 -0400

    instantiated models for Link Project and Screenshot

--- back-end/users/models.py
+++ back-end/users/models.py
@@ -3,11 +3,39 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
   username = models.CharField(max_length=120)
-  projects = None # I think is is null until we have the applicable foreign model
-  bio = CharField(max_length=255, null=True)
+  projects = models.ForeignKey(Project, related_name='users', null=True)
+  bio = models.CharField(max_length=255, null=True)
   profile_pic = models.URLField(null=True)
-  links = None # I think is is null until we have the applicable foreign model
+  links = models.ForeignKey(Link, related_name='users', null=True)
   location = models.CharField(max_length=120, null=True)
 
   def __str__(self):
     return self.username
+
+
+class Link(models.Model):
+  username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+  url = models.URLField()
+
+  def __str__(self):
+    return self.username # is username the correct choice here?
+
+
+class Project(models.Model):
+  project_name = models.CharField(max_length=120)
+  username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+  description = models.CharField(max_length=255, null=True)
+  projects_website = models.URLField(null=True)
+  screenshots = models.ForeignKey(Screenshot, related_name='users', null=True)
+  source_code = models.URLField(null=True)
+  technologies_used = models.CharField(max_length=120, null=True)
+
+  def __str__(self):
+    return self.username
+
+class Screenshot(models.Model):
+  project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='users')
+  screenshot = models.URLField()
+
+  def __str__(self):
+    return self.project
