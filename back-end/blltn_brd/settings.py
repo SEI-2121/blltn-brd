55d7014423d699d17d06d85ef5ce01a2d0d89a14
Dave Easley <easley.david@gmail.com>
2021-04-14 09:11:21 -0400

    rmvd commented out key

--- back-end/blltn_brd/settings.py
+++ back-end/blltn_brd/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3h-mu$poajmce()*wai19#l3nut-4cbs_^9hwj_!q6cltuim^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
