57a2ae0760b43a8408e14d722d8522862ffe8819
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-04-14 10:39:26 -0400

    corrected foregien key relations and individualized dunder str return values

--- back-end/users/models.py
+++ back-end/users/models.py
@@ -3,10 +3,8 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
   username = models.CharField(max_length=120)
-  projects = models.ForeignKey(Project, related_name='users', null=True)
   bio = models.CharField(max_length=255, null=True)
   profile_pic = models.URLField(null=True)
-  links = models.ForeignKey(Link, related_name='users', null=True)
   location = models.CharField(max_length=120, null=True)
 
   def __str__(self):
@@ -14,28 +12,28 @@ class User(models.Model):
 
 
 class Link(models.Model):
-  username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
-  url = models.URLField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+  link_name = models.CharField(max_length=120)
+  link_url = models.URLField()
 
   def __str__(self):
-    return self.username # is username the correct choice here?
-
+    return self.link_name
 
 class Project(models.Model):
   project_name = models.CharField(max_length=120)
-  username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
   description = models.CharField(max_length=255, null=True)
   projects_website = models.URLField(null=True)
-  screenshots = models.ForeignKey(Screenshot, related_name='users', null=True)
   source_code = models.URLField(null=True)
   technologies_used = models.CharField(max_length=120, null=True)
 
   def __str__(self):
-    return self.username
+    return self.project_name
 
 class Screenshot(models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='users')
+  screenshot_name = models.CharField(max_length=120)
   screenshot = models.URLField()
 
   def __str__(self):
-    return self.project
+    return self.screenshot_name
