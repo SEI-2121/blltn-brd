623294dedda4d09c9c73df750b59471e2738b96c
Joshua <jws.maker@gmail.com>
2021-04-15 12:23:42 -0600

    resolving app duplication error & test

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App/App.js';
+import App from './pages/MainApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
