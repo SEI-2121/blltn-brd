4123a38fb6341bb1973a12c9db92e33e9a80fb65
stacy leitstein <unique523@yahoo.com>
2021-04-19 13:38:17 -0400

    changed Navbar to logo

--- front-end/src/pages/MainApp/Navbar/index.js
+++ front-end/src/pages/MainApp/Navbar/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Navbar, Nav, Button, FormControl, Form } from 'react-bootstrap'
 import './Nav.css'
+import image from '../../../images/favicon.ico'
+
 function Navigation() {
+
+   
     return (
         <div>
             <Navbar className="color" variant="dark">
-                <Navbar.Brand href="/home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="/">{<img src={image} alt="Logo" />}</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/user">User</Nav.Link>
                     <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/testing">Testing</Nav.Link>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
