ecb9dad800d772257c42349ec3c0439121896254
stacy leitstein <unique523@yahoo.com>
2021-04-19 09:39:18 -0400

    moved Navbar to MainApp as per Josh' request

--- front-end/src/pages/MainApp/Navbar/index.js
+++ front-end/src/pages/MainApp/Navbar/index.js
@@ -7,8 +7,9 @@ function Navigation() {
             <Navbar className="color" variant="dark">
                 <Navbar.Brand href="/home">Navbar</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/user-details">User</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/user">User</Nav.Link>
                     <Nav.Link href="/login">Login</Nav.Link>
                 </Nav>
                 <Form inline>
