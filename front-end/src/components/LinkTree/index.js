5bee6ca4b948bbf3502bf9c98ea9ea59d1770206
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-04-19 15:54:52 -0400

    social link tree instatiated

---
+++ front-end/src/components/LinkTree/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+
+export default class LinkTree extends Component {
+  render() {
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>Social Links</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item action href="#link1">Link 1</ListGroup.Item>
+          <ListGroup.Item action href="#link1">Link 2</ListGroup.Item>
+          <ListGroup.Item action href="#link1">Link 3</ListGroup.Item>
+          <ListGroup.Item action href="#link1">Link 4</ListGroup.Item>
+        </ListGroup>
+      </Card>
+    )
+  }
+}
