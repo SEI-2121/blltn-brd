db233ac4b78e2e9e9ba1378577606dac2c28c2ff
dshearwood <d.algorhythmic@gmail.com>
2021-04-19 13:57:22 -0400

    Added About Page

--- front-end/src/pages/MainApp/About.js
+++ front-end/src/pages/MainApp/About.js
@@ -1,23 +1,46 @@
 import React from "react";
-import "./About.css"
+import "./About.css";
 
 export default function About() {
   return (
     <div>
       <div class="container">
-        <h3>- Our Mission -</h3>
-        <h6>
-          Changing the way people think about porfolio websites. Spark the
-          revolution!
-        </h6>
+        
+        {/* <h3><span class="color">-</span> Our Mission <span class="color">-</span></h3> */}
+        <h1>
+          Changing the way people think about porfolio websites.</h1>
+          <h1> SPARK THE <span class="color">REVOLUTION</span>!
+          <hr></hr>
+        </h1>
 
-        <h3>About</h3>
-        <h6>
-          Foundedn in 2021, Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          consectetur, quaerat maiores, deleniti labore cumque minus in
-          provident enim nesciunt porro amet accusantium molestias officiis
-          culpa nihil. Modi, aliquid explicabo?{" "}
-        </h6>
+        <h3><span class="color">-</span> Our Vision <span class="color">-</span></h3>
+        <h5>
+          At Blltn Brd, we realize how to maximize the
+          micro-interaction. Brave enough to target the commonly-accepted
+          paradigms of the fractal relationship. A company that envisioneers
+          correctly and upgrades the world's capacity to incentivize synergy.
+          What do we enable? Anything and everything.
+        </h5>
+
+        <h3><span class="color">-</span> A New Era of Thought Leadership <span class="color">-</span></h3>
+        <h5>
+          What does it really mean to cultivate "the portfolio"? Think
+          compelling, 24/7. Think 60/24/7/365. Think virtual. But don't think
+          all three at the same time. Blltn Brd has revamped the
+          conceptualization of experiences. We enable the capability of eyeballs
+          to actualize. Without well-planned niches, micro-interactions are
+          forced to become backwards-thinking.
+        </h5>
+
+        <h3><span class="color">-</span> Expect the Unexpected <span class="color">-</span></h3>
+        <h5>
+          The markets factor can be summed up in one word: subscriber-defined,
+          cross-platform, real-world. A company that can expedite correctly will
+          (at some undefined point of time in the future) be able to grow
+          fiercely. A company that can architect fiercely will (one day) be able
+          to orchestrate fiercely. What does the term "cyber-bleeding-edge"
+          really mean? Let's find out.
+        </h5>
       </div>
     </div>
   );
