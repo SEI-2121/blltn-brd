198e55e86a4a4153f2867fb502a9cdd53d9ee0b5
Erika Sanchez <erika.walker.business@gmail.com>
2021-04-20 19:02:46 -0400

    login page stuff

---
+++ front-end/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../../components/Common/Homepage/Login.js'
+
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            <Login />
+        </div>
+    )
+}
