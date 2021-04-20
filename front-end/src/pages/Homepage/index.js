4a039e28d5124c0ec18fac3710341abb1fa21ee4
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-04-20 10:26:04 -0400

    homepage tree rearranged

---
+++ front-end/src/pages/Homepage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Signup from '../../components/Common/Homepage/Signup'
+import Login from '../../components/Common/Homepage/Login.js'
+
+
+
+export default function HomePage() {
+    return (
+      <div>
+        <Login />
+        <Signup />
+        </div>
+
+    )
+  }
--- front-end/src/pages/MainApp/index.js
+++ front-end/src/pages/MainApp/index.js
@@ -3,15 +3,16 @@ import "./style.css";
 import UserDetail from "../UserDetail";
 import Testing from "../Testing";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import HomePage from '../../components/Homepage/index.js';
+import HomePage from '../Homepage/index.js';
 import Navbar from '../../components/Common/Navbar';
+import Header from '../../components/Common/Homepage/Header.js';
 
 export default function App() {
   return (
     <div>
-
       <Router>
         <div>
+          <Header />
           <Navbar />
            <Switch>
             <Route path="/about">
