e5f191b0a63b69c53807c824515b28ea80f93d5a
Erika Sanchez <erika.walker.business@gmail.com>
2021-04-23 10:19:18 -0400

    Changes to make sure it works

--- front-end/src/pages/NewProject/index.js
+++ front-end/src/pages/NewProject/index.js
@@ -3,14 +3,14 @@ import './style.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import NewProject from '../../components/ProjectCard/NewProject.js'
 
-export default function NewProjectPage() {
+export default function NewProjectPage(props) {
     return (
         <Container>
             <Row>
                 <Col><h1>New Project</h1></Col>
             </Row>
             <Row>
-                <Col><NewProject {...props} /></Col>
+                <Col><NewProject { ...props } /></Col>
             </Row>
         </Container>
     )
